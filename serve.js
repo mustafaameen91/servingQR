45a95a9b49a0c58653d8498c67f537e83b95ae32
mustafaameen91 <mustafa.mohammed.ameen91@gmail.com>
2021-04-04 23:01:03 +0300

    first commit

---
+++ serve.js
@@ -0,0 +1,61 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const fs = require("fs");
+
+app.use(cors());
+app.use(express.json({ limit: "50mb" }));
+
+app.get("/api/certificates/:file", function (request, response) {
+   let file = request.params.file;
+   var extension = file.split(".").pop();
+   var tempFile = `./certificates/${file}`;
+   fs.readFile(tempFile, function (err, data) {
+      console.log(extension);
+      switch (extension) {
+         case "jpg":
+            contentType = "image/jpg";
+            isImage = 1;
+            break;
+         case "png":
+            contentType = "image/png";
+            isImage = 1;
+            break;
+         case "jpeg":
+            contentType = "image/jpeg";
+            isImage = 1;
+            break;
+         case "pdf":
+            contentType = "application/pdf";
+            isImage = 2;
+            break;
+      }
+      response.contentType(contentType);
+      response.send(data);
+   });
+});
+
+app.post(`/uploadCertificate`, (req, res) => {
+   let base64Data = req.body.pdf.replace(/^data:application\/pdf;base64,/, "");
+
+   fs.writeFile(
+      `./certificates/${req.body.fileName}.pdf`,
+      base64Data,
+      "base64",
+      function (err) {
+         if (err) {
+            res.status(500).send({
+               message:
+                  err.message ||
+                  "Some error occurred while creating the patient.",
+            });
+         } else {
+            res.send({ message: "saved file" });
+         }
+      }
+   );
+});
+
+app.listen(3350, () => {
+   console.log("Server is running on port 3350.");
+});
